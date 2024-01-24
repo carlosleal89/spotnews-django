23c2819564b648544e4e3f243b4473c2a50703d1
Carlos Leal <carlos.frleal@gmail.com>
2024-01-23 22:36:53 -0300

    req02

--- news/models.py
+++ news/models.py
@@ -6,3 +6,13 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return f'{self.name}'
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    email = models.EmailField(max_length=200, blank=False)
+    password = models.CharField(max_length=200, blank=False)
+    role = models.CharField(max_length=200, blank=False)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
