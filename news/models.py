25188f393459d3f60d61cfa1ef03e7fe08e17cfa
Carlos Leal <carlos.frleal@gmail.com>
2024-01-23 23:01:27 -0300

    req03 em andamento

--- news/models.py
+++ news/models.py
@@ -16,3 +16,15 @@ class User(models.Model):
 
     def __str__(self) -> str:
         return f'{self.name}'
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200, blank=False)
+    content = models.CharField(blank=False)
+    author = models.ForeignKey('User')
+    created_at = models.DateField(blank=False)
+    image = models.ImageField(upload_to='img/', blank=True)
+    categories = models.ForeignKey('Category')
+
+    def __str__(self) -> str:
+        return f'{self.title}'
