65195bcf91ea43692bf9a4d614c5a1b018f4a0e5
Carlos Leal <carlos.frleal@gmail.com>
2024-01-26 14:09:00 -0300

    req04

---
+++ news/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from news.views import index
+
+urlpatterns = [
+  path("", index, name="home-page")
+]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from news.models import News
 
-# Create your views here.
+
+def index(request):
+    context = {"news": News.objects.all()}
+    return render(request, 'home.html', context)
